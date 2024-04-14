17fe7befb2a6e45dd0822d0ef4eb2d3e4b43ae24
Lior1254 <liorshemtov2008@gmail.com>
2024-04-14 20:21:10 +0300

    first page and secend page xml prepared, need to work on the calculation

--- app/src/main/java/com/example/matriculation_calculator/MainActivity.java
+++ app/src/main/java/com/example/matriculation_calculator/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.matriculation_calculator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE = 69;
     EditText user_eD,eD1,eD2,eD3,eD4,eD5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,119 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Next_page(View view) {
-
+        String st1 = eD1.getText().toString();
+        String st2 = eD1.getText().toString();
+        String st3 = eD1.getText().toString();
+        String st4 = eD1.getText().toString();
+        String st5 = eD1.getText().toString();
+        String st6 = user_eD.getText().toString();
+        if(!st6.isEmpty())
+        {
+            if (!st1.isEmpty() & !st2.isEmpty() & !st3.isEmpty() & !st4.isEmpty() & !st5.isEmpty()) {
+                int num1 = Integer.parseInt(st1);
+                int num2 = Integer.parseInt(st2);
+                int num3 = Integer.parseInt(st3);
+                int num4 = Integer.parseInt(st4);
+                int num5 = Integer.parseInt(st5);
+                if (check_legal(num1) & check_legal(num2) & check_legal(num3) & check_legal(num4) & check_legal(num5)) {
+                    Intent si = new Intent(this, Second_screen.class);
+                    double sum = (num1 + num2 + num3 + num4 + num5) / 5;
+                    si.putExtra("sum",sum);
+                    startActivity(si);
+                } else {
+                    Toast.makeText(this, "The input is illegal!", Toast.LENGTH_SHORT);
+                }
+            } else {
+                Toast.makeText(this, "One of the inputs is Empty", Toast.LENGTH_SHORT);
+            }
+        }
+        else
+        {
+            Toast.makeText(this, "User Name is Empty!", Toast.LENGTH_SHORT);
+        }
     }
+
+    public boolean check_legal(int num)
+    {
+
+        if(num <= 100)
+        {
+            return true;
+        }
+        return false;
+    }
+
 }
+
+
+/*
+ @Override
+    protected void onActivityResult(int source, int result, @Nullable Intent data_back){
+        super.onActivityResult(source, result, data_back);
+        if (source == REQUEST_CODE) {
+            if (result == Activity.RESULT_OK) {
+                if (data_back != null) {
+                    b3 = data_back.getBooleanExtra("b1",false);
+                    b4 = data_back.getBooleanExtra("b2",false);
+                    tv1.setText("");
+                    tv4.setText("");
+
+                    if(b4)
+                    {
+                        ans2 = data_back.getDoubleExtra("X2", -1);
+                        tv4.setText(String.format("X: %.1f",ans2));
+                    }
+
+
+                    if(b3)
+                    {
+                        ans1 = data_back.getDoubleExtra("X1", -1);
+                        tv1.setText(String.format("X: %.1f",ans1));
+                    }
+                    if(b4 || b3)
+                    {
+                        iV.setVisibility(View.VISIBLE);
+                        if(a > 0)//+a  u
+                        {
+                            if (check_variation(a,b,c) > 0)
+                            {
+                                iV.setImageResource(R.drawable.abc_a_minus_b);
+                            }
+                            else if (check_variation(a,b,c) == 0)
+                            {
+                                iV.setImageResource(R.drawable.abc_a);
+                            }
+                            else
+                            {
+                                iV.setImageResource(R.drawable.abc_nox_a);
+                            }
+                        }
+                        else if (a < 0)//-a h
+                        {
+                            if (check_variation(a,b,c) > 0)
+                            {
+                                iV.setImageResource(R.drawable.abc_ninus_a_b);
+                            }
+                            else if (check_variation(a,b,c) == 0)
+                            {
+                                iV.setImageResource(R.drawable.abc_minus_a);
+                            }
+                            else
+                            {
+                                iV.setImageResource(R.drawable.abc_nox_minus_a);
+                            }
+                        }
+                        else //-----
+                        {
+                            iV.setImageResource(R.drawable.abc);
+                        }
+                    }
+                    else
+                    {
+                        iV.setVisibility(View.INVISIBLE);
+                    }
+                }
+            }
+        }
+    }
+ */
--- app/src/main/java/com/example/matriculation_calculator/Second_screen.java
+++ app/src/main/java/com/example/matriculation_calculator/Second_screen.java
@@ -1,6 +1,9 @@
 package com.example.matriculation_calculator;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +12,62 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Second_screen extends AppCompatActivity {
-
+    EditText eD_1_1,eD_1_2,eD_1_3,eD_2_1,eD_2_2,eD_2_3,eD_3_1,eD_3_2,eD_3_3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_screen);
+        eD_1_1 = findViewById(R.id.eD_1_1);
+        eD_1_2 = findViewById(R.id.eD_1_2);
+        eD_1_3 = findViewById(R.id.eD_1_3);
+        eD_2_1 = findViewById(R.id.eD_2_1);
+        eD_2_2 = findViewById(R.id.eD_2_2);
+        eD_2_3 = findViewById(R.id.eD_2_3);
+        eD_3_1 = findViewById(R.id.eD_3_1);
+        eD_3_2 = findViewById(R.id.eD_3_2);
+        eD_3_3 = findViewById(R.id.eD_3_3);
+
+        Intent gi = getIntent();
+        double sum = gi.getDoubleExtra("sum",-1);
+    }
+
+    public void first_op(View view) {
+        eD_1_1.setVisibility(View.VISIBLE);
+        eD_1_2.setVisibility(View.VISIBLE);
+        eD_1_3.setVisibility(View.VISIBLE);
+        eD_2_1.setVisibility(View.INVISIBLE);
+        eD_2_2.setVisibility(View.INVISIBLE);
+        eD_2_3.setVisibility(View.INVISIBLE);
+        eD_3_1.setVisibility(View.INVISIBLE);
+        eD_3_2.setVisibility(View.INVISIBLE);
+        eD_3_3.setVisibility(View.INVISIBLE);
+    }
+
+    public void second_op(View view) {
+        eD_1_1.setVisibility(View.VISIBLE);
+        eD_1_2.setVisibility(View.VISIBLE);
+        eD_1_3.setVisibility(View.VISIBLE);
+        eD_2_1.setVisibility(View.VISIBLE);
+        eD_2_2.setVisibility(View.VISIBLE);
+        eD_2_3.setVisibility(View.VISIBLE);
+        eD_3_1.setVisibility(View.INVISIBLE);
+        eD_3_2.setVisibility(View.INVISIBLE);
+        eD_3_3.setVisibility(View.INVISIBLE);
+    }
+
+    public void third_op(View view) {
+        eD_1_1.setVisibility(View.VISIBLE);
+        eD_1_2.setVisibility(View.VISIBLE);
+        eD_1_3.setVisibility(View.VISIBLE);
+        eD_2_1.setVisibility(View.VISIBLE);
+        eD_2_2.setVisibility(View.VISIBLE);
+        eD_2_3.setVisibility(View.VISIBLE);
+        eD_3_1.setVisibility(View.VISIBLE);
+        eD_3_2.setVisibility(View.VISIBLE);
+        eD_3_3.setVisibility(View.VISIBLE);
+    }
+
+    public void go_back(View view) {
+        finish();
     }
 }
